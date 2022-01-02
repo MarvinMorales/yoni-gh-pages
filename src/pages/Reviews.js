110d4d6bc649c99794f67e7c020d79b9ed89ce14
Marvin Morales <marvmustaf@gmail.com>
2022-01-02 00:45:58 -0500

    Open Graph

--- src/pages/Reviews.js
+++ src/pages/Reviews.js
@@ -25,7 +25,7 @@ export const Reviews = () => {
                                     <h4 className="city">Los Angeles, CA</h4>
                                     <div className="review-stars">{ Array(5).fill(<i className="fas fa-star"></i>) }</div>
                                 </div>
-                                <div className="imagesHome"><img className="h-images" src={require("../images/home1.jpg")}/></div>
+                                <div className="imagesHome"><img alt="Kyle C." className="h-images" src={require("../images/home1.jpg")}/></div>
                             </div>
                             <div className="review-body">
                             Wow! Made buying a house such a breeze! I was new to home buying, 
@@ -42,7 +42,7 @@ export const Reviews = () => {
                                     <h4 className="city">Riverside, CA</h4>
                                     <div className="review-stars">{ Array(5).fill(<i className="fas fa-star"></i>) }</div>
                                 </div>
-                                <div className="imagesHome"><img className="h-images" src={require("../images/home2.jpg")}/></div>
+                                <div className="imagesHome"><img alt="Jonathan M." className="h-images" src={require("../images/home2.jpg")}/></div>
                             </div>
                             <div className="review-body">
                             Yoni and their team were extremely helpful, open, transparent, and reliable through the whole process, they really cared about us, understood our needs from the very first open house even giving us ideas on how better utilize the space.
@@ -56,7 +56,7 @@ export const Reviews = () => {
                                     <h4 className="city">Moreno Valley, CA</h4>
                                     <div className="review-stars">{ Array(5).fill(<i className="fas fa-star"></i>) }</div>
                                 </div>
-                                <div className="imagesHome"><img className="h-images" src={require("../images/home3.jpg")}/></div>
+                                <div className="imagesHome"><img alt="Keith B." className="h-images" src={require("../images/home3.jpg")}/></div>
                             </div>
                             <div className="review-body">
                             Yoni is absolutely wonderful to work with. It’s like having a friend on the “inside” that is working for you. He really care about his/hers clients as well as the community and neighborhood we live in.
@@ -70,7 +70,7 @@ export const Reviews = () => {
                                     <h4 className="city">Marrieta, GA</h4>
                                     <div className="review-stars">{ Array(5).fill(<i className="fas fa-star"></i>) }</div>
                                 </div>
-                                <div className="imagesHome"><img className="h-images" src={require("../images/home4.jpg")}/></div>
+                                <div className="imagesHome"><img alt="Gabriel N." className="h-images" src={require("../images/home4.jpg")}/></div>
                             </div>
                             <div className="review-body">
                             Yoni was more than helpful in finding us a buyer for our apartment he was extremely honest, upfront, and was able to find us a good way to get rid of the tenant! He was extremely communicative and easy to reach to answer any of our questions at all times. Yoni made he nightmare turn into heaven!
@@ -84,7 +84,7 @@ export const Reviews = () => {
                                     <h4 className="city">Los Angeles, CA</h4>
                                     <div className="review-stars">{ Array(5).fill(<i className="fas fa-star"></i>) }</div>
                                 </div>
-                                <div className="imagesHome"><img className="h-images" src={require("../images/home5.jpg")}/></div>
+                                <div className="imagesHome"><img alt="Steve C." className="h-images" src={require("../images/home5.jpg")}/></div>
                             </div>
                             <div className="review-body">
                             Yoni was very helpful for us in finding a new home and selling or distressed one in short, he was always on our side working to make our house purchase as simple and successful as possible. The best part about working with Joe was that he was always more focused on answering our questions, giving us good advice, and finding homes that met our needs than he was on closing a deal. We would recommend him to anyone.
@@ -98,7 +98,7 @@ export const Reviews = () => {
                                     <h4 className="city">Huntington Park, CA</h4>
                                     <div className="review-stars">{ Array(5).fill(<i className="fas fa-star"></i>) }</div>
                                 </div>
-                                <div className="imagesHome"><img className="h-images" src={require("../images/home6.jpeg")}/></div>
+                                <div className="imagesHome"><img alt="Jose A." className="h-images" src={require("../images/home6.jpeg")}/></div>
                             </div>
                             <div className="review-body">
                             Throughout my adult years, I have had the experience of purchasing a few different houses. As my family began to grow, I recently found the need and desire to purchase a larger home. I spoke to a variety of different realtors that showed me a variety of homes until I found Yoni Rios, When making such a large decision, I wanted to work with someone that I could trust. 
@@ -108,11 +108,11 @@ export const Reviews = () => {
                         <div className="review-box">
                             <div className="review-header">
                                 <div>
-                                    <h3 className="review-name">John b.</h3>
+                                    <h3 className="review-name">John B.</h3>
                                     <h4 className="city">Los Angeles, CA</h4>
                                     <div className="review-stars">{ Array(5).fill(<i className="fas fa-star"></i>) }</div>
                                 </div>
-                                <div className="imagesHome"><img className="h-images" src={require("../images/home7.jpg")}/></div>
+                                <div className="imagesHome"><img alt="John B." className="h-images" src={require("../images/home7.jpg")}/></div>
                             </div>
                             <div className="review-body">
                             Yoni and his team worked with us from mid-August identifying the right buyer for our property He was always accessible and willing to make a variety of suggestions and options as we tried to. Are the right decision. We feel we have a trustworthy investor as well as a friend! Thank you Yoni!  
