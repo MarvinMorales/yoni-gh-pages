9acc8cfea400cb7a85d903ac2ae7f72860f47170
Marvin Morales <marvmustaf@gmail.com>
2022-01-05 15:38:22 -0500

    Updating About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,8 @@ export const About = () => {
                     <div className="ourClientsInner">Who we are & <span className="span1">what</span> do we do?</div>
                 </div>
                 <article className="reviews-container">
-                   <div className="about-container">
+                    <div className="yoni-container"><img src={require("../images/Yoni.jfif")}/></div>
+                   <div className="about-container-2">
                     We are local family owned investors who actively invest in Real Estate. <br/> <br/>
                     We have done thousands of transactions and understand the process of selling 
                     a house, which might be the hardest transaction for a family.  <br/> <br/>
@@ -18,7 +19,7 @@ export const About = () => {
                     We want to make this process as smooth as possible, working around your time frame and terms that work mutually for
                     both parties. We have extended experience in real estate and will guide you throughout the process as we do what
                     we believe is the best for you, putting your interest before ours. We look forward to talking to you 
-                    and making this process a smooth one for you and your family..
+                    and making this process a smooth one for you and your family.
                    </div>
                 </article>
             </section>
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -16,9 +16,9 @@ export const Header = (props) => {
                 <li onClick={() => {setHeader("Reviews"); setMenu(false); navigate('reviews')}}>Reviews</li>
             </ul>
             <ul className="menu1">
-                <li onClick={() => {setHeader("Home"); navigate('/')}} style={{borderBottom:`4px solid rgba(0, 140, 255, ${header === "Home" ? 1 : 0})`, color:`${header === "Home" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>Home</li>
-                <li onClick={() => {setHeader("About"); navigate('/about')}} style={{borderBottom:`4px solid rgba(0, 140, 255, ${header === "About" ? 1 : 0})`, color:`${header === "About" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>About</li>
-                <li onClick={() => {setHeader("Reviews"); navigate('reviews')}} style={{borderBottom:`4px solid rgba(0, 140, 255, ${header === "Reviews" ? 1 : 0})`, color:`${header === "Reviews" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>Reviews</li>
+                <li onClick={() => {setHeader("Home"); navigate('/')}} style={{borderBottom:`1px solid rgba(0, 140, 255, ${header === "Home" ? 1 : 0})`, color:`${header === "Home" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>Home</li>
+                <li onClick={() => {setHeader("About"); navigate('/about')}} style={{borderBottom:`1px solid rgba(0, 140, 255, ${header === "About" ? 1 : 0})`, color:`${header === "About" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>About</li>
+                <li onClick={() => {setHeader("Reviews"); navigate('reviews')}} style={{borderBottom:`1px solid rgba(0, 140, 255, ${header === "Reviews" ? 1 : 0})`, color:`${header === "Reviews" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>Reviews</li>
             </ul>
             <h1 style={{display:"none"}}>We buy houses in every state</h1>
             <h2 style={{display:"none"}}>We buy houses in every state</h2>
