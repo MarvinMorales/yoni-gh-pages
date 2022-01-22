78e4d07f3c2bce70f2c28425fe2714e54ac8aa85
Ashish Kumar <ashish.kumar@iris-worldwide.com>
2022-01-22 12:06:15 +0530

    home page css fixes and service page added

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -13,11 +13,13 @@ export const Header = (props) => {
             <ul className={menu ? "menu2 animationIn" : "menu2 animationOut"}>
                 <li onClick={() => {setHeader("Home"); setMenu(false); navigate('/')}}>Home</li>
                 <li onClick={() => {setHeader("About"); setMenu(false); navigate('/about')}}>About</li>
+                <li onClick={() => {setHeader("Service"); setMenu(false); navigate('/service')}}>Service</li>
                 <li onClick={() => {setHeader("Reviews"); setMenu(false); navigate('reviews')}}>Reviews</li>
             </ul>
             <ul className="menu1">
                 <li onClick={() => {setHeader("Home"); navigate('/')}} style={{borderBottom:`1px solid rgba(0, 140, 255, ${header === "Home" ? 1 : 0})`, color:`${header === "Home" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>Home</li>
                 <li onClick={() => {setHeader("About"); navigate('/about')}} style={{borderBottom:`1px solid rgba(0, 140, 255, ${header === "About" ? 1 : 0})`, color:`${header === "About" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>About</li>
+                <li onClick={() => {setHeader("Service"); navigate('/service')}} style={{borderBottom:`4px solid rgba(0, 140, 255, ${header === "Service" ? 1 : 0})`, color:`${header === "Service" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>Service</li>
                 <li onClick={() => {setHeader("Reviews"); navigate('reviews')}} style={{borderBottom:`1px solid rgba(0, 140, 255, ${header === "Reviews" ? 1 : 0})`, color:`${header === "Reviews" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>Reviews</li>
             </ul>
             <h1 style={{display:"none"}}>We buy houses in every state</h1>
