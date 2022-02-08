1a2c0b21f2bc9fe4d04d0e163eeec46e439fce8d
Ashish Kumar <ashish.kumar@iris-worldwide.com>
2022-02-09 00:25:00 +0530

    call-to-action-button

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -13,6 +13,7 @@ export const Header = (props) => {
             <Link to={"/"} className="brand_logo">
             <img src={require('../images/logo.png')} />
             </Link>
+            <div className="nav_wrap">
             <i className="fas fa-bars menuIcon" onClick={() => setMenu(!menu)}></i>
             <ul className={menu ? "menu2 animationIn" : "menu2 animationOut"}>
                 <li onClick={() => {setHeader("Home"); setMenu(false); navigate('/')}}>Home</li>
@@ -21,6 +22,7 @@ export const Header = (props) => {
                 <li onClick={() => {setHeader("Reviews"); setMenu(false); navigate('/reviews')}}>Reviews</li>
                  <li onClick={() => {setHeader("Locations"); setMenu(false); navigate('/locations')}}>Locations</li>
                  <li onClick={() => {setHeader("Blogs"); setMenu(false); navigate('/blogs')}}>Blogs</li>
+                 <li><a href="tel:9493255813" className="call_button">call: 9493255813</a></li>
             </ul>
             <ul className="menu1">
                 <li onClick={() => {setHeader("Home"); navigate('/')}} style={{borderBottom:`1px solid rgba(0, 140, 255, ${header === "Home" ? 1 : 0})`, color:`${header === "Home" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>Home</li>
@@ -29,7 +31,9 @@ export const Header = (props) => {
                 <li onClick={() => {setHeader("Reviews"); navigate('/reviews')}} style={{borderBottom:`1px solid rgba(0, 140, 255, ${header === "Reviews" ? 1 : 0})`, color:`${header === "Reviews" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>Reviews</li>
                  <li onClick={() => {setHeader("Locations"); navigate('/locations')}} style={{borderBottom:`1px solid rgba(0, 140, 255, ${header === "Locations" ? 1 : 0})`, color:`${header === "Locations" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>Locations</li>
                 <li onClick={() => {setHeader("Blogs"); navigate('/blogs')}} style={{borderBottom:`1px solid rgba(0, 140, 255, ${header === "Blogs" ? 1 : 0})`, color:`${header === "Blogs" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>Blogs</li>
+                <li><a href="tel:9493255813" className="call_button">call: 9493255813</a></li>
             </ul>
+            </div>
             <h1 style={{display:"none"}}>We buy houses in every state</h1>
             <h2 style={{display:"none"}}>We buy houses in every state</h2>
       </header>
