c3214d5a60f65fa52098162203ff49432947e0aa
Ashish Kumar <ashish.kumar@iris-worldwide.com>
2022-03-03 17:13:16 +0530

    menu link

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink} from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 export const Header = (props) => {
@@ -16,22 +16,23 @@ export const Header = (props) => {
             <div className="nav_wrap">
             <i className="fas fa-bars menuIcon" onClick={() => setMenu(!menu)}></i>
             <ul className={menu ? "menu2 animationIn" : "menu2 animationOut"}>
-                <li onClick={() => {setHeader("Home"); setMenu(false); navigate('/')}}>Home</li>
-                <li onClick={() => {setHeader("About"); setMenu(false); navigate('/about')}}>About</li>
-                <li onClick={() => {setHeader("Service"); setMenu(false); navigate('/service')}}>Service</li>
-                <li onClick={() => {setHeader("Reviews"); setMenu(false); navigate('/reviews')}}>Reviews</li>
-                 <li onClick={() => {setHeader("Locations"); setMenu(false); navigate('/locations')}}>Locations</li>
-                 <li onClick={() => {setHeader("Blogs"); setMenu(false); navigate('/blogs')}}>Blogs</li>
-                 <li><a href="tel:9493255813" className="call_button">Call: 9493255813</a></li>
+            <li onClick={() => {setMenu(false);}}><NavLink to={'/'} className="header-nav__nav-link">Home</NavLink></li>
+            <li onClick={() => {setMenu(false);}}><NavLink to={'/about'} className="header-nav__nav-link">About</NavLink></li>
+            <li onClick={() => {setMenu(false);}}><NavLink to={'/service'} className="header-nav__nav-link">Service</NavLink></li>
+            <li onClick={() => {setMenu(false);}}><NavLink to={'/reviews'} className="header-nav__nav-link">Reviews</NavLink></li>
+            <li onClick={() => {setMenu(false);}}><NavLink to={'/locations'} className="header-nav__nav-link">Locations</NavLink></li>
+            <li onClick={() => {setMenu(false);}}><NavLink to={'/blogs'} className="header-nav__nav-link">Blogs</NavLink></li>
+            <li onClick={() => {setMenu(false);}}><a href="tel:9493255813" className="call_button">Call: 9493255813</a></li>
+
             </ul>
             <ul className="menu1">
-                <li onClick={() => {setHeader("Home"); navigate('/')}} style={{borderBottom:`1px solid rgba(0, 140, 255, ${header === "Home" ? 1 : 0})`, color:`${header === "Home" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>Home</li>
-                <li onClick={() => {setHeader("About"); navigate('/about')}} style={{borderBottom:`1px solid rgba(0, 140, 255, ${header === "About" ? 1 : 0})`, color:`${header === "About" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>About</li>
-                <li onClick={() => {setHeader("Service"); navigate('/service')}} style={{borderBottom:`4px solid rgba(0, 140, 255, ${header === "Service" ? 1 : 0})`, color:`${header === "Service" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>Service</li>
-                <li onClick={() => {setHeader("Reviews"); navigate('/reviews')}} style={{borderBottom:`1px solid rgba(0, 140, 255, ${header === "Reviews" ? 1 : 0})`, color:`${header === "Reviews" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>Reviews</li>
-                 <li onClick={() => {setHeader("Locations"); navigate('/locations')}} style={{borderBottom:`1px solid rgba(0, 140, 255, ${header === "Locations" ? 1 : 0})`, color:`${header === "Locations" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>Locations</li>
-                <li onClick={() => {setHeader("Blogs"); navigate('/blogs')}} style={{borderBottom:`1px solid rgba(0, 140, 255, ${header === "Blogs" ? 1 : 0})`, color:`${header === "Blogs" ? "#29A8DF" : "rgb(82, 82, 82)"}`}}>Blogs</li>
-                <li><a href="tel:323-553-5700" className="call_button">Call: 323-553-5700</a></li>
+            <li><NavLink to={'/'} className="header-nav__nav-link">Home</NavLink></li>
+            <li><NavLink to={'/about'} className="header-nav__nav-link">About</NavLink></li>
+            <li><NavLink to={'/service'} className="header-nav__nav-link">Service</NavLink></li>
+            <li><NavLink to={'/reviews'} className="header-nav__nav-link">Reviews</NavLink></li>
+            <li><NavLink to={'/locations'} className="header-nav__nav-link">Locations</NavLink></li>
+            <li><NavLink to={'/blogs'} className="header-nav__nav-link">Blogs</NavLink></li>
+            <li><a href="tel:9493255813" className="call_button">Call: 9493255813</a></li>
             </ul>
             </div>
             <h1 style={{display:"none"}}>We buy houses in every state</h1>
