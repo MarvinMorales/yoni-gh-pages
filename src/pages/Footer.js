ea21b1e62fcfd360d36a9ef128e4f1ade20a9e81
Ashish Kumar <ashish.kumar@iris-worldwide.com>
2022-03-08 02:13:18 +0530

    footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Footer = () => {
-    return (<footer className='footer2'>&copy;Copyright www.sellmyhousequickfast.com 2022 - All rights reserved for more information related with us use the contact option</footer>);
+    return (<footer className='footer2'>&copy;Copyright www.sellmyhousequickfast.com 2022 - All rights reserved for more information related with us use the contact option <span>Made By <a href="https://creativesquadz.com/" target="_blank">Creative Squadz</a></span></footer>);
 }
